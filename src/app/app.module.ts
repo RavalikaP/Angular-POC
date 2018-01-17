bd4e7786d1cacb315055351006c8f9af23ae7f67
Ravalika Panchakarla <ravalika.panchakarla@inrhythm-inc.ind>
2018-01-17 18:09:21 +0530

    Rename of component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UserDetailsComponent } from './user-details/user-details.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserDetailsComponent
+    MenuBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/shared/user.interface.ts
@@ -0,0 +1,8 @@
+export interface User {
+    userId: number;
+    name: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+};
